1abc92f2173afa053e13749aaa4890a3f9ce36fe
Vikram P <125629713+Vikram05vikky@users.noreply.github.com>
2024-09-20 22:31:44 +0530

    Create number of even digits.cpp

---
+++ math/number of even digits.cpp
@@ -0,0 +1,28 @@
+class Solution {
+
+    int finddigit(int n)
+    {
+        int count=0;
+
+        while(n>0)
+        {
+            int a = n%10;
+            count++;
+            n=n/10;
+
+        }
+        return count;
+    }
+
+public:
+    int findNumbers(vector<int>& nums) {
+        int a=0;
+        for(auto it : nums)
+        {
+            int digit = finddigit(it);
+            if(digit%2==0)
+                a++;
+        }
+        return a;        
+    }
+};
