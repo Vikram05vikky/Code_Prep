7777f90ed1686791f2bfaf0325fef78b5b8040e9
Vikram P <125629713+Vikram05vikky@users.noreply.github.com>
2024-10-16 09:10:17 +0530

    Update Intersection of Two Linked Lists.cpp

--- Linked List/Intersection of Two Linked Lists.cpp
+++ Linked List/Intersection of Two Linked Lists.cpp
@@ -39,3 +39,38 @@ public:
         return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//         ListNode* temp=headA;
+
+//         while(temp)
+//         {
+//             temp->val= -1 * (temp->val);
+//             temp=temp->next;
+//         }
+
+//         ListNode* ans=NULL;
+//         ListNode* temp1=headB;
+
+//         while(temp1)
+//         {
+//             if(temp1->val < 0)
+//             {
+//                 ans=temp1;
+//                 break;
+//             }
+//             temp1=temp1->next;
+            
+//         }
+//         temp = headA;
+//         while (temp) {
+//             temp->val = -1 * (temp->val); 
+//             temp = temp->next;
+//         }
+
+//         return ans;
+//     }
+// };
