bb2800b26cd1b042bf65ecf96ded4394de0ba5ef
Vikram P <125629713+Vikram05vikky@users.noreply.github.com>
2025-01-24 09:49:15 +0530

    Create Binary Tree.cpp

---
+++ Tree/Binary Tree.cpp
@@ -0,0 +1,68 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int data)
+    {
+        this->data=data;
+        left=right=nullptr;
+    }
+};
+// Node* root=nullptr;
+Node* constructTree( vector<int>& arr)
+{
+    Node* root=new Node(arr[0]);
+    int ind=1;
+    queue<Node*>q;
+    q.push(root);
+    while(!q.empty() && ind<arr.size())
+    {
+        Node* current=q.front();
+        q.pop();
+        if(ind<arr.size() && arr[ind]!=-1)
+        {
+            current->left=new Node(arr[ind]);
+            q.push(current->left);
+        }
+
+        ind++;
+        if(ind<arr.size() && arr[ind]!=-1)
+        {
+            current->right=new Node(arr[ind]);
+            q.push(current->right);
+        }
+
+        ind++;
+    }
+    return root;
+}
+void traversal(Node* root)
+{
+    queue<Node*>q;
+    q.push(root);
+    while(!q.empty())
+    {
+        Node* current=q.front();
+        q.pop();
+        cout<<current->data<<" ";
+        if(current->left)
+        {
+            q.push(current->left);
+        }
+        if(current->right)
+        {
+            q.push(current->right);
+        }
+    }
+}
+int main()
+{
+    vector<int>arr={1,2,3,4,-1,-1,5};
+    Node* root=constructTree(arr);
+    traversal(root);
+}
